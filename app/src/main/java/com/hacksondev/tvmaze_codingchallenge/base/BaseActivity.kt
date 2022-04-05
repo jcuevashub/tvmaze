d7b71194ff745b9addb236682b694414c8b2696c
Jackson Cuevas <jackson.cuevas@hotmail.com>
2022-04-05 14:58:58 -0400

    remove unnecessary files

--- app/src/main/java/com/hacksondev/tvmaze_codingchallenge/base/BaseActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.hacksondev.tvmaze_codingchallenge.base
-
-import androidx.lifecycle.ViewModel
-
-abstract class BaseActivity<VM : ViewModel> : BaseBindingActivity() {
-
-    protected abstract val viewModel: VM
-}
